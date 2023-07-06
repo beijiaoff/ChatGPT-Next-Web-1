fd88d9d979c89dac0d0a236e832bea53ca1943f0
beijiaoff <sxbqf@163.com>
2023-07-06 16:10:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          机械室内部使用，请勿传播。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
